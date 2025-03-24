9030832d66fa30a3d98b5a4a45cf2fb0fe235d5d
AmandeepSNG <amandeepstglt@gmail.com>
2025-03-24 10:06:21 +0530

    Context API implementation

---
+++ week-7/day-1/context-api/src/components/children/child_3.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SubChild_3 from '../subChildren/subChild_3'
+
+function Child_3() {
+  return (
+    <div>
+      <p>Hello I am child 3</p>
+      <SubChild_3/>
+    </div>
+  )
+}
+
+export default Child_3
